02a2257a4cac6fa606228cb00e402fac53c21bad
Nanda Vikas Konduru <nandavikas858@gmail.com>
2023-04-14 18:54:11 +0530

    added env file

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
+const port = process.env.PORT || 8080;
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/shopping-app'
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/shopping-app', { useNewUrlParser: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
     .then(() => {
         console.log('MongoDB connected');
-        return server.listen({ port: 8080 });
+        return server.listen({ port });
     }
 )
 
